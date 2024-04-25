d7f07a82dc2ba6e0ec79a4fdbb43057925ff681e
Jbopp3 <jmbopp3@gmail.com>
2024-04-25 17:47:31 -0400

    4/25

---
+++ src/interface.py
@@ -0,0 +1,20 @@
+import PySimpleGUI as sg
+
+sg.theme('Blue')   # Add a touch of color
+# All the stuff inside your window.
+layout = [ 
+            [sg.Text('Enter Your Search'), sg.InputText()],
+            [sg.Button('Search')],
+]
+
+# Create the Window
+window = sg.Window('Test', layout).Finalize()
+#window.Maximize()
+# Event Loop to process "events" and get the "values" of the inputs
+while True:
+    event, values = window.read()
+    if event in (None, 'Close Window'): # if user closes window or clicks cancel
+        break
+    print('You entered ', values[0])
+
+window.close()
